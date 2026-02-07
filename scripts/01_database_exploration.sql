e9906c50703e77ed63efe4352e7359906ae7ab98
MD. ALAMIN MIA <116589620+alaminl570@users.noreply.github.com>
2026-02-08 00:05:00 +0600

    Add files via upload

---
+++ scripts/01_database_exploration.sql
@@ -0,0 +1,51 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+-- Explore all objects in the Database
+SELECT * FROM INFORMATION_SCHEMA.TABLES;
+
+
+
+-- Explore all columns in the Database
+
+--Explore 'customers' Tables all columns
+
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'customers';
+
+
+--Explore 'products' Tables all columns
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'products';
+
+
+--Explore 'sales' Tables all columns
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'sales';
+
