eca039810be2c475b67e04ad81583fc337a69e0f
MD. ALAMIN MIA <116589620+alaminl570@users.noreply.github.com>
2026-02-14 16:52:39 +0600

    Add files via upload

---
+++ scripts/06_ranking_analysis.sql
@@ -0,0 +1,63 @@
+/*
+===============================================================================
+Ranking Analysis
+===============================================================================
+Purpose:
+    - To rank items (e.g., products, customers) based on performance or other metrics.
+    - To identify top performers or laggards.
+
+SQL Functions Used:
+    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
+    - Clauses: GROUP BY, ORDER BY
+===============================================================================
+*/
+
+-- Which 5 Products generate the highest revenue
+SELECT TOP 5
+    p.product_name,
+    SUM (s.sales_amount) AS total_revenue
+FROM sales s
+LEFT JOIN products p
+ON p.product_key = s.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- What are the 5 worst-performing products in term of sales?
+SELECT TOP 5
+    p.product_name,
+    SUM (s.sales_amount) AS total_revenue
+FROM sales s
+LEFT JOIN products p
+ON p.product_key = s.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC;
+
+-- Find the top 10 customers who have generated the highest revenue
+SELECT TOP 10
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    SUM(s.sales_amount) AS total_revenue
+FROM sales s
+LEFT JOIN customers c
+    ON c.customer_key = s.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_revenue DESC;
+
+-- The 3 customers with the fewest orders placed
+SELECT TOP 3
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    COUNT(DISTINCT order_number) AS total_orders
+FROM sales s
+LEFT JOIN customers c
+    ON c.customer_key = s.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_orders ;
